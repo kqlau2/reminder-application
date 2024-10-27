bd7a35b99ecd6c519261a15fc311fa7c28b69b04
kqlau2 <kqlau1@gmail.com>
2024-10-27 18:17:14 +0800

    learn react query

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from './components/Home';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import DisplayPost from './components/DisplayPost';
+import CreatePost from './components/CreatePost';
 
 import './App.css';
 import userpool from './userpool';
@@ -20,10 +22,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path='/reminder-application' element={<Home />}/>
         <Route path='/signup' element={<Signup />}/>
         <Route path='/login' element={<Login />}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/displayPost" element={<DisplayPost/>}/>
+        <Route path='/createPost' element={<CreatePost/>}/>
       </Routes>
     </BrowserRouter>
   );
